b50676cab576b7577d9c9dbcc4c22052bc606d2d
araRina <inserttheemailyouusehere@gmail.com>
2020-11-12 03:19:33 -0500

    Add Medicine View Model

---
+++ app/src/main/java/com/example/pillreminderapp/datastorage/MedicineViewModel.java
@@ -0,0 +1,25 @@
+package com.example.pillreminderapp.datastorage;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class MedicineViewModel extends AndroidViewModel {
+
+    private MedicineRepository mRepository;
+
+    private final LiveData<List<Medicine>> mAllMedicines;
+
+    public MedicineViewModel (Application application) {
+        super(application);
+        mRepository = new MedicineRepository(application);
+        mAllMedicines = mRepository.getAllMedicines();
+    }
+
+    LiveData<List<Medicine>> getAllMedicines() { return mAllMedicines; }
+
+    public void insert(Medicine Medicine) { mRepository.insert(Medicine); }
+}
