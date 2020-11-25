48b5ad3ddeda6eb548a9964e3e1fa6b2ff132a6f
spaldingra <spaldingra@vcu.edu>
2020-11-24 23:01:43 -0500

    Revert "Finally got the notification working how it should"
    This reverts commit b4970b402a8b021cfd33baf8c1fd483ff5deeced.

--- app/src/main/java/com/example/pillreminderapp/activities/NewMedicineActivity.java
+++ app/src/main/java/com/example/pillreminderapp/activities/NewMedicineActivity.java
@@ -1,55 +1,37 @@
 package com.example.pillreminderapp.activities;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.DialogInterface;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.telephony.TelephonyManager;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.example.pillreminderapp.R;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
 
-public class NewMedicineActivity extends AppCompatActivity {
-    private static final int REQUEST_CODE = 101;
-    String IMEINumber="",token="";
+public class NewMedicineActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
+{
+
     public static final String EXTRA_REPLY = "com.example.android.Medicinelistsql.REPLY";
+
     private EditText mEditMedicineView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_medicine);
         mEditMedicineView = findViewById(R.id.edit_Medicine);
+
         final Button button = findViewById(R.id.button_save);
         button.setOnClickListener(view -> {
             Intent replyIntent = new Intent();
@@ -59,104 +41,19 @@ public class NewMedicineActivity extends AppCompatActivity {
                 String Medicine = mEditMedicineView.getText().toString();
                 replyIntent.putExtra(EXTRA_REPLY, Medicine);
                 setResult(RESULT_OK, replyIntent);
-                gettokenagainstimei(IMEINumber);
             }
             finish();
         });
-        getDeviceIMEI();
-    }
-    @SuppressLint("HardwareIds")
-    public void getDeviceIMEI() {
-        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        if (ActivityCompat.checkSelfPermission(NewMedicineActivity.this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(NewMedicineActivity.this, new String[]{Manifest.permission.READ_PHONE_STATE}, REQUEST_CODE);
-            return;
-        }
-        IMEINumber = telephonyManager.getDeviceId();
-    }
-    public void gettokenagainstimei(final String imei) {
-        StringRequest request = new StringRequest(Request.Method.POST, "http://www.studentfyp.online/getimeitoken.php", new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    Log.d("res",response);
-                    JSONObject object = new JSONObject(response);
-                    JSONArray complaints = object.getJSONArray("result");
-                    for (int i = 0; i < complaints.length(); i++) {
-                        Log.d("complaints", complaints + "");
-                        JSONObject object1 = complaints.getJSONObject(i);
-                        String token = object1.getString("token");
-                        Log.d("tokenthis",token);
-                        sendnotification(token);
-                        finishAndRemoveTask();
-                    }
-                } catch (JSONException e) {
 
-                    //  CustomProgressDialog.hide();
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.medicine_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                // CustomProgressDialog.hide();
-            }
-        }) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-//                return super.getParams();
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("imei", "353288080775442" + "");
-                return params;
-            }
-        };
-        RequestQueue rQueue = Volley.newRequestQueue(NewMedicineActivity.this);
-        rQueue.add(request);
     }
-    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_CODE: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "Permission granted.", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(this, "Permission denied.", Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
-    }
-    public void sendnotification(final String token)
-    {
-        StringRequest request = new StringRequest(Request.Method.POST,"http://www.studentfyp.online/empiremessage.php", new Response.Listener<String>(){
-            @Override
-            public void onResponse(String response) {
-                Log.d("respone",response);
-            }
-        },new Response.ErrorListener(){
 
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
-//                Toast.makeText(Quiz.this, "Error: ", Toast.LENGTH_SHORT).show();
-                new AlertDialog.Builder(NewMedicineActivity.this).setMessage("An error Occured! Please try again!").setTitle("Alert!").setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                }).show();
-                Log.d("verror","Error: "+volleyError.getMessage());
-            }
-        }){
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> parameters = new HashMap<String, String>();
-                parameters.put("token",""+token);
-                return parameters;
-            }
-        };
-        RequestQueue rQueue = Volley.newRequestQueue(NewMedicineActivity.this);
-        rQueue.add(request);
-    }
-    public String getTimeNow() {
+    public String getTimeNow (){
 
         String currentTime;
 
@@ -164,9 +61,9 @@ public class NewMedicineActivity extends AppCompatActivity {
 
     }
 
-    public boolean checkTime(String timeNow, String timeMed) {
+    public boolean checkTime (String timeNow, String timeMed){
 
-        if (timeNow.equals(timeMed)) {
+        if (timeNow.equals(timeMed)){
 
             return true;
 
@@ -179,13 +76,18 @@ public class NewMedicineActivity extends AppCompatActivity {
     }
 
     //Method helps save selected item in drop down menu
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long l)
+    {
         String item = parent.getItemAtPosition(position).toString();
         Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
     }
 
-    public void onNothingSelected(AdapterView<?> adapterView) {
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView)
+    {
 
     }
-
 }
+
+
