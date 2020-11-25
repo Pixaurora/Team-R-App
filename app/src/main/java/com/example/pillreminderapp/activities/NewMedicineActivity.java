5ef7e9f92ab37848598b77760fb3f8d02be2e0ad
Deven Sharma <sharmad9@vcu.edu>
2020-11-24 22:49:11 -0500

    Date/Time stuff for demo branch

--- app/src/main/java/com/example/pillreminderapp/activities/NewMedicineActivity.java
+++ app/src/main/java/com/example/pillreminderapp/activities/NewMedicineActivity.java
@@ -2,20 +2,27 @@ package com.example.pillreminderapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.pillreminderapp.R;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
@@ -25,6 +32,10 @@ public class NewMedicineActivity extends AppCompatActivity implements AdapterVie
     public static final String EXTRA_REPLY = "com.example.android.Medicinelistsql.REPLY";
 
     private EditText mEditMedicineView;
+    DatePickerDialog pickerDate;
+    EditText eText;
+    Button btnGet;
+    TextView tvw;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,7 +62,44 @@ public class NewMedicineActivity extends AppCompatActivity implements AdapterVie
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
+
+        TimePicker picker=(TimePicker)findViewById(R.id.timePicker1);
+        picker.setIs24HourView(false);
+
+        tvw=(TextView)findViewById(R.id.textView1);
+        eText=(EditText) findViewById(R.id.editText1);
+        eText.setInputType(InputType.TYPE_NULL);
+        eText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+
+                pickerDate = new DatePickerDialog(NewMedicineActivity.this,
+                        new DatePickerDialog.OnDateSetListener()
+                        {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+                            {
+                                eText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                pickerDate.show();
+            }
+        });
+        btnGet=(Button)findViewById(R.id.button1);
+        btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvw.setText("Selected Date: "+ eText.getText());
+            }
+        });
     }
+
+
+
 
     public String getTimeNow (){
 
