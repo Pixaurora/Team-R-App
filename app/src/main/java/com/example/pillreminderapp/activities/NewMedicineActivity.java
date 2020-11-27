6fb007c7e889fed5a62c22aa002a9f0a4bd20c87
araRina <inserttheemailyouusehere@gmail.com>
2020-11-26 20:45:47 -0500

    make it so it can compile again

--- app/src/main/java/com/example/pillreminderapp/activities/NewMedicineActivity.java
+++ app/src/main/java/com/example/pillreminderapp/activities/NewMedicineActivity.java
@@ -71,7 +71,7 @@ public class NewMedicineActivity extends AppCompatActivity {
                 String Medicine = mEditMedicineView.getText().toString();
                 replyIntent.putExtra(EXTRA_REPLY, Medicine);
                 setResult(RESULT_OK, replyIntent);
-                gettokenagainstimei(IMEINumber);
+//                gettokenagainstimei(IMEINumber);
             }
             finish();
         });
