2e279a6afc28a0619ec9969c4e9be6b52c18acc8
araRina <inserttheemailyouusehere@gmail.com>
2020-11-27 20:07:40 -0500

    Add time converter and test for that.

---
+++ app/src/main/java/com/example/pillreminderapp/TimeStringConverter.java
@@ -0,0 +1,23 @@
+package com.example.pillreminderapp;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class TimeStringConverter {
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static String TimeToString(LocalTime time) {
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("HH:mm:ss");
+        return df.format(time);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static LocalTime StringToTime(String timeString) {
+        LocalTime time = LocalTime.parse(timeString);
+
+        return time;
+    }
+}
